c6cce315a97bcb1a1f94248c471f75f7a5d3700b
pd212 <deshpandepranav47@gmail.com>
2023-07-29 18:23:13 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,24 @@ import React, {Component, useState,useEffect} from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [showParagraph,setShowParagraph] = useState("");
-
-  function showPara(){
-    setShowParagraph("Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy");
-  }
-
+  const [clicked, setClicked] = useState(false);
 
   return (
-    <div className="App" id="main">
-      // Do not alter the main div
-     <p>{showParagraph}</p>
-     <button id="click" onClick={showPara} >Show Paragraph</button>
+    <div id="main">
+      <button
+        id="click"
+        onClick={() => {
+          setClicked(!clicked);
+        }}
+      >
+        Click
+      </button>
+      {clicked && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
